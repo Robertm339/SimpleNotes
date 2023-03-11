709a5e5fbf36e82f9f5e04cb2e3d63f25f2d8d85
Robertm339 <71312299+Robertm339@users.noreply.github.com>
2023-03-10 20:55:04 -0800

    Initial Commit

---
+++ SimpleNotes/SimpleNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNotesApp.swift
+//  SimpleNotes
+//
+//  Created by Robert Martinez on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
